24476b4662ade5d875f2a7ec5af729323a0220fe
filmaj <maj.fil@gmail.com>
2021-03-18 15:43:01 -0400

    actually adding tests and ci

---
+++ test/index-test.js
@@ -0,0 +1,46 @@
+const plugin = require('../');
+const { join } = require('path');
+const inventory = require('@architect/inventory');
+const fs = require('fs-extra');
+const sampleDir = join(__dirname, '..', 'sample-app');
+const appDir = join(__dirname, 'tmp');
+const originalCwd = process.cwd();
+const cfn = { Resources: {
+  HTTP: { Properties: { DefinitionBody: { paths: {} } } },
+  Role: { Properties: { Policies: [] } }
+} };
+
+describe('plugin packaging function', () => {
+  let inv = {};
+  let arc = {};
+  beforeAll(async () => {
+    // Set up integration test directory as a copy of sample app
+    const appPluginDir = join(appDir, 'node_modules', 'arc-plugin-s3-image-bucket');
+    await fs.mkdirp(appPluginDir);
+    await fs.copy(join(sampleDir, 'app.arc'), join(appDir, 'app.arc'));
+    await fs.copy(join(__dirname, '..', 'index.js'), join(appPluginDir, 'index.js'));
+    process.chdir(appDir);
+    inv = await inventory({});
+    arc = inv.inv._project.arc;
+  });
+  afterAll(async () => {
+    process.chdir(originalCwd);
+    await fs.remove(appDir);
+  });
+  describe('when not present in project', () => {
+    it('should not modify the CloudFormation JSON', () => {
+      const app = { ...arc };
+      delete app['image-bucket'];
+      const output = plugin.package({ arc: app, cloudformation: cfn, inventory: inv });
+      expect(JSON.stringify(output)).toBe(JSON.stringify(cfn));
+    });
+  });
+  describe('when present in project', () => {
+    it('should create a lambda function definition for each rule defined in the arc manifest', () => {
+      const app = { ...arc };
+      const output = plugin.package({ arc: app, cloudformation: cfn, inventory: inv, stage: 'staging' });
+      expect(output.Resources.ImageBucketOnImageCreatePluginLambda).toBeDefined();
+      expect(output.Resources.ImageBucketOnImageCreatePluginLambda.Properties.Layers.length).toEqual(1);
+    });
+  });
+});
